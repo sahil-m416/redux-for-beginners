036be3e3f8f6e9b82ba351cf0fc75ed89ca80999
sahil <sahilsaify7860@gmail.com>
2022-07-27 17:28:04 +0530

    basic redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//! create store will take one thing i.e., reducer
+
+// // STORE -> Is a globalised state, holds all the data of our application...
+// it is accessible everywhere
+
+// // ACTION -> describes what you want to do
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+// // REDUCER -> How your action is going to modify you last store into new store
+
+const counter = (state=0, action) => {
+    switch(action.type){
+      case 'INCREMENT':
+        return state+1
+      case 'DECREMENT':
+        return state-1
+      default:
+        return 0
+    }
+}
+
+
+let store = createStore(counter);
+store.subscribe(() => console.log(store.getState()))
+// // DISPATCH -> where we can actually emit command to hit the store
+store.dispatch(increment())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
